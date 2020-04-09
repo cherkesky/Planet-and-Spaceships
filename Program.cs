d728a7672c7a93643516acfd2f8db37efcf2c21e
Cherkesky <guy@cherkesky.com>
2020-04-09 14:50:28 -0500

    exercise 1-3

--- Program.cs
+++ Program.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 
 namespace lists
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        List<string> planetList = new List<string>(){"Mercury", "Mars"};
-        }
+      List<string> planetList = new List<string>() { "Mercury", "Mars" };
+    planetList.Add("Jupiter");
+    planetList.Add("Saturn");
+
+    List<string> extraPlanets = new List<string>() { "Earth", "Pluto" };
+      planetList.AddRange(extraPlanets);
+      Console.WriteLine("");
+       foreach (var item in planetList)
+      {
+        Console.Write(" ");
+        Console.Write(item);
+      }
     }
+  }
 }
