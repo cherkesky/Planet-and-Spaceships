9c2d7f4902a7948d37751c9320dc18872602a09d
Cherkesky <guy@cherkesky.com>
2020-04-09 15:12:30 -0500

    exercises 3-7

--- Program.cs
+++ Program.cs
@@ -8,17 +8,36 @@ namespace lists
     static void Main(string[] args)
     {
       List<string> planetList = new List<string>() { "Mercury", "Mars" };
-    planetList.Add("Jupiter");
-    planetList.Add("Saturn");
+      planetList.Add("Jupiter");
+      planetList.Add("Saturn");
 
-    List<string> extraPlanets = new List<string>() { "Earth", "Pluto" };
+      List<string> extraPlanets = new List<string>() { "Uranus", "Neptune" };
       planetList.AddRange(extraPlanets);
-      Console.WriteLine("");
-       foreach (var item in planetList)
+      planetList.Insert(0, "Earth");
+      planetList.Insert(3, "Venus");
+      planetList.Add("Pluto");
+
+      List<string> rockyPlanets = new List<string>();
+      rockyPlanets = planetList.GetRange(2, 3);
+
+      planetList.Remove("Pluto");
+
+      Console.WriteLine("\nplanetList");
+      foreach (var item in planetList)
       {
         Console.Write(" ");
         Console.Write(item);
       }
+      
+      Console.WriteLine("\nrockyPlanets");
+      foreach (var item in rockyPlanets)
+      {
+        Console.Write(" ");
+        Console.Write(item);
+      }
+
+      Console.WriteLine("\n Length: {0} {1}", planetList.Count, rockyPlanets.Count);
+    
     }
   }
 }
